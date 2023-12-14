0154848b9896ae02892529ca6f605facbfa674d6
kayy0812 <levanloc8112@gmail.com>
2023-12-14 22:36:26 +0700

    updated

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__) . '/TruyenTranh/autoload.php';
 use TruyenTranh\Main;
 
 // Thông tin kết nối cơ sở dữ liệu
-define("DB_HOST", 'localhost');
+define("DB_HOST", '127.0.0.1');
 define("DB_PORT", '3306');
 define("DB_NAME", 'db_truyentranh');
 define("DB_USER", 'root');
@@ -12,3 +12,4 @@ define("DB_PASS", '');
 
 $main = new Main();
 
+print_r($main->query("SELECT * FROM comics"));
